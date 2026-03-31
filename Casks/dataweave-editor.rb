e6ff46504c943657e53b57633ffff88feca874d0
Christof Walther <christof.walther@gmail.com>
2026-03-31 10:34:24 +0200

    fix: use correct asset filenames (dot not space)

--- Casks/dataweave-editor.rb
+++ Casks/dataweave-editor.rb
@@ -3,12 +3,12 @@ cask "dataweave-editor" do
 
   on_arm do
     sha256 "b13359059b152cf98ef1ed01c448ebcfb1b80bc77c77c1fd96d3007d94372542"
-    url "https://github.com/chrises5/dataweave-editor/releases/download/v#{version}/DataWeave%20Editor-#{version}-arm64-mac.zip"
+    url "https://github.com/chrises5/dataweave-editor/releases/download/v#{version}/DataWeave.Editor-#{version}-arm64-mac.zip"
   end
 
   on_intel do
     sha256 "eccc87412b7197dc905c599e2c59352ed6209c2a1ccda553004b35fd07a41db9"
-    url "https://github.com/chrises5/dataweave-editor/releases/download/v#{version}/DataWeave%20Editor-#{version}-x64-mac.zip"
+    url "https://github.com/chrises5/dataweave-editor/releases/download/v#{version}/DataWeave.Editor-#{version}-x64-mac.zip"
   end
 
   name "DataWeave Editor"
